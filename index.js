3d14d72499ce0a0bbbe75c8fe7802caf80d457ff
syeda-sunjida <nikitaferdous1999@gmail.com>
2023-06-10 05:30:52 +0600

    admin info

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -9,6 +10,22 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhlikac.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,6 +43,73 @@ async function run() {
     const classCollection = client.db("singerella").collection("classes");
     const instructorCollection = client.db("singerella").collection("instructors");
     const enrolledCollection = client.db("singerella").collection("enrolled");
+    const usersCollection = client.db("singerella").collection("users");
+
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+  
+        res.send({ token })
+      });
+
+      // Warning: use verifyJWT before using verifyAdmin
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        if (user?.role !== 'admin') {
+          return res.status(403).send({ error: true, message: 'forbidden message' });
+        }
+        next();
+      }
+
+      // users related apis
+    app.get('/users',verifyJWT, verifyAdmin, async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      })
+
+      app.post('/users', async (req, res) => {
+        const user = req.body; const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query);
+  
+        if (existingUser) {
+          return res.send({ message: 'user already exists' })
+        }
+  
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+      // security layer: verifyJWT
+    // email same
+    // check admin
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+        const email = req.params.email;
+  
+        if (req.decoded.email !== email) {
+          res.send({ admin: false })
+        }
+  
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' }
+        res.send(result);
+      })
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+  
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+  
+      });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -40,6 +124,28 @@ async function run() {
         const result = await instructorCollection.find().toArray();
         res.send(result);
       })
+
+
+      // cart collection apis
+    app.get('/enrolled', verifyJWT, async (req, res) => {
+
+        const email = req.query.email;
+        // console.log(email)
+        if (!email) {
+          res.send([]);
+        }
+        const decodedEmail = req.decoded.email;
+        if (email !== decodedEmail) {
+          return res.status(403).send({ error: true, message: 'porviden access' })
+        }
+        else {
+          const query = { email: email };
+          const result = await enrolledCollection.find(query).toArray();
+          res.send(result);
+        }
+  
+      });
+
       app.delete('/enrolled/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
