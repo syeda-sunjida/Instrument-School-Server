957afafad80247b8395657911a110084c90b9ecb
syeda-sunjida <nikitaferdous1999@gmail.com>
2023-06-10 19:35:17 +0600

    post method for usrs

--- index.js
+++ index.js
@@ -10,22 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// const verifyJWT = (req, res, next) => {
-//     const authorization = req.headers.authorization;
-//     if (!authorization) {
-//       return res.status(401).send({ error: true, message: 'unauthorized access' });
-//     }
-//     // bearer token
-//     const token = authorization.split(' ')[1];
-  
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//       if (err) {
-//         return res.status(401).send({ error: true, message: 'unauthorized access' })
-//       }
-//       req.decoded = decoded;
-//       next();
-//     })
-//   }
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhlikac.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -45,32 +30,14 @@ async function run() {
     const enrolledCollection = client.db("singerella").collection("enrolled");
     const usersCollection = client.db("singerella").collection("users");
 
-    // app.post('/jwt', (req, res) => {
-    //     const user = req.body;
-    //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-  
-    //     res.send({ token })
-    //   });
-
-      // Warning: use verifyJWT before using verifyAdmin
-    //   const verifyAdmin = async (req, res, next) => {
-    //     const email = req.decoded.email;
-    //     const query = { email: email }
-    //     const user = await usersCollection.findOne(query);
-    //     if (user?.role !== 'admin') {
-    //       return res.status(403).send({ error: true, message: 'forbidden message' });
-    //     }
-    //     next();
-    //   }
-
-      // users related apis
-    // app.get('/users', async (req, res) => {
-    //     const result = await usersCollection.find().toArray();
-    //     res.send(result);
-    //   })
+    
 
       app.post('/users', async (req, res) => {
-        const user = req.body; const query = { email: user.email }
+        console.log('Received POST request at /users');
+  console.log('Request body:', req.body);
+
+        const user = req.body;
+         const query = { email: user.email }
         const existingUser = await usersCollection.findOne(query);
   
         if (existingUser) {
@@ -78,6 +45,7 @@ async function run() {
         }
   
         const result = await usersCollection.insertOne(user);
+        console.log('User inserted:', result);
         res.send(result);
       });
 
@@ -107,7 +75,7 @@ async function run() {
       });
 
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
 // get classes data
     app.get('/classes', async (req, res) => {
@@ -120,32 +88,21 @@ async function run() {
         res.send(result);
       })
 
+      app.get('/enrolled', async (req, res) => {
+        const result = await enrolledCollection.find().toArray();
+        res.send(result);
+      })
 
-      // cart collection apis
-    // app.get('/enrolled', verifyJWT, async (req, res) => {
 
-    //     const email = req.query.email;
-    //     // console.log(email)
-    //     if (!email) {
-    //       res.send([]);
-    //     }
-    //     const decodedEmail = req.decoded.email;
-    //     if (email !== decodedEmail) {
-    //       return res.status(403).send({ error: true, message: 'porviden access' })
-    //     }
-    //     else {
-    //       const query = { email: email };
-    //       const result = await enrolledCollection.find(query).toArray();
-    //       res.send(result);
-    //     }
-  
-    //   });
 
-      app.delete('/enrolled/:id', async (req, res) => {
-        const id = req.params.id;
+      //  collection apis
+   
+      app.delete('/enrolled/:_id', async (req, res) => {
+        const id = req.params._id;
         const query = { _id: new ObjectId(id) };
         const result = await enrolledCollection.deleteOne(query);
         res.send(result);
+        console.log(result)
       })
 
       app.post('/enrolled', async (req, res) => {
